f6ca964cc48f9206867c2fa43a63ae03fe415964
Muhamad Rifqi Ramadhan <rifqyomp@gmail.com>
2023-10-27 17:17:06 +0700

    initial commit

---
+++ src/test/java/com/enigma/wmb/WarungMakanBahariApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.enigma.wmb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WarungMakanBahariApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
